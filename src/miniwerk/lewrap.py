99babfa3d9828c8e61f8f87e307c1cbd9de75dbf
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-10-02 21:58:01 +0300

    add note about certbot doing horrible things to root logger

--- src/miniwerk/lewrap.py
+++ src/miniwerk/lewrap.py
@@ -38,5 +38,7 @@ def call_certbot(config: MWConfig) -> List[str]:
         return args
 
     LOGGER.debug("Calling certbot_main({})".format(args))
+    # FIXME: This overrides root logger level to debug (whyyyy)
+    #        we probably are better off just launching a shell afterall
     certbot_main(args)
     return args
