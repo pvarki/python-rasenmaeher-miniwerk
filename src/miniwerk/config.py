7653609799dfc53ae0d92dc771c671aae4339efd
Eero af Heurlin <eero.afheurlin@iki.fi>
2025-06-28 18:48:01 +0300

    fix: use just mtx as the name and set the playback port as user port

--- src/miniwerk/config.py
+++ src/miniwerk/config.py
@@ -37,7 +37,7 @@ class MWConfig(BaseSettings):
     le_test: bool = Field(default=True, description="Use LE staging/test env")
     subdomains: str = Field(default="mtls", description="Comma separated list of extra subdomains to get certs for")
     products: str = Field(
-        default="fake,tak,bl,rmmtx",
+        default="fake,tak,bl,mtx",
         description="Comma separated list of products to create manifests and get subdomains for",
     )
     fake: ProductSettings = Field(
@@ -55,9 +55,9 @@ class MWConfig(BaseSettings):
         description="Setting for BattleLog integration API",
         default_factory=lambda: ProductSettings(api_host="bl", user_host="bl", api_port=4626, user_port=4626),
     )
-    rmmtx: ProductSettings = Field(
+    mtx: ProductSettings = Field(
         description="Setting for MediaMTX integration API",
-        default_factory=lambda: ProductSettings(api_host="rmmtx", user_host="rmmtx", api_port=4626, user_port=4626),
+        default_factory=lambda: ProductSettings(api_host="mtx", user_host="mtx", api_port=4626, user_port=9888),
     )
 
     le_cert_name: str = Field(default="rasenmaeher", description="--cert-name for LE, used to determine directory name")
