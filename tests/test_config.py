a72c0fdb2f290a6fa691496aef88aa68e1fa3134
BOSSKEK3 <228652459+BOSSKEK3@users.noreply.github.com>
2025-12-18 10:33:51 +0200

    fix: add airguardian to config test

--- tests/test_config.py
+++ tests/test_config.py
@@ -16,7 +16,7 @@ def test_defaults() -> None:
     assert cfg.ci is True
     assert cfg.domain == "pytest.pvarki.fi"
     assert cfg.subdomains == "mtls"
-    assert cfg.products == "fake,tak,bl,mtx,matrix"
+    assert cfg.products == "fake,tak,bl,mtx,matrix,airguardian"
     assert str(cfg.data_path) != "/data/persistent"
     assert cfg.le_email == "example@example.com"
     LOGGER.debug("cfg.fqdns={}".format(cfg.fqdns))
@@ -35,6 +35,8 @@ def test_defaults() -> None:
         "mtx.pytest.pvarki.fi",
         "mtls.matrix.pytest.pvarki.fi",
         "matrix.pytest.pvarki.fi",
+        "mtls.airguardian.pytest.pvarki.fi",
+        "airguardian.pytest.pvarki.fi",
     }
     assert cfg.keytype is KeyType.ECDSA
 
