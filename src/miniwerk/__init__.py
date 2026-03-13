dcc4b2abcaeaf9503cccf46e453bfbb0fddcbc9a
vitamiini-0 <228652369+vitamiini-0@users.noreply.github.com>
2026-03-13 07:54:04 +0200

    chore: bump version

--- src/miniwerk/__init__.py
+++ src/miniwerk/__init__.py
@@ -1,3 +1,3 @@
 """Minimal KRAFTWERK amulation to be able to run a RASENMAEHER+products deployment on any VM"""
 
-__version__ = "1.6.0"  # NOTE Use `bump2version --config-file patch` to bump versions correctly
+__version__ = "1.7.0"  # NOTE Use `bump2version --config-file patch` to bump versions correctly
