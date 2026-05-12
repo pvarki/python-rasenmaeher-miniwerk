03ddf69543aa43f6c8965aaa3f5d29b3a08186c3
Akseli Lehtniemi <60463103+Akaseli@users.noreply.github.com>
2026-05-12 13:57:06 +0300

    chore: bump version

--- src/miniwerk/__init__.py
+++ src/miniwerk/__init__.py
@@ -1,3 +1,3 @@
 """Minimal KRAFTWERK amulation to be able to run a RASENMAEHER+products deployment on any VM"""
 
-__version__ = "1.7.1+260427"  # NOTE Use `bump-my-version` to bump versions correctly
+__version__ = "1.8.0+260512"  # NOTE Use `bump-my-version` to bump versions correctly
