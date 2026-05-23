d2dd6e41ff961b3a064d3ea040699e3d68455a28
Eero af Heurlin <eero.afheurlin@iki.fi>
2026-05-23 13:37:45 +0300

    chore: bump version

--- src/miniwerk/__init__.py
+++ src/miniwerk/__init__.py
@@ -1,3 +1,3 @@
 """Minimal KRAFTWERK amulation to be able to run a RASENMAEHER+products deployment on any VM"""
 
-__version__ = "1.8.0+260512"  # NOTE Use `bump-my-version` to bump versions correctly
+__version__ = "1.8.1+260523"  # NOTE Use `bump-my-version` to bump versions correctly
