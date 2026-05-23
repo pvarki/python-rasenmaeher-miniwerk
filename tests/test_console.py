3023f2751086c58db6aea170a28d35c3c957bb78
Eero af Heurlin <eero.afheurlin@iki.fi>
2026-05-23 13:49:17 +0300

    fix: add test for CLI entrypoint to generate manifests

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,11 +1,13 @@
 """Test CLI scripts"""
 
 import asyncio
+import os
 
 import pytest
 from libadvian.binpackers import ensure_str
 
 from miniwerk import __version__
+from miniwerk.config import MWConfig
 
 
 @pytest.mark.asyncio
@@ -22,3 +24,22 @@ async def test_version_cli():  # type: ignore
     assert process.returncode == 0
     # Check output
     assert ensure_str(out[0]).strip().endswith(__version__)
+
+
+@pytest.mark.asyncio
+async def test_manifest_cli():  # type: ignore
+    """Test the CLI command for dumping manifests works"""
+    conf = MWConfig()
+    cmd = "miniwerk manifests"
+    process = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+        env=os.environ,
+    )
+    await asyncio.wait_for(process.communicate(), 10)
+    # Demand clean exit
+    assert process.returncode == 0
+    # Check output
+    for manifile in conf.product_manifest_paths.values():
+        assert manifile.exists()
