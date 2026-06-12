9adb59b866f769710fbaa8a5c610f5f14c0b88bb
Eero af Heurlin <eero.afheurlin@iki.fi>
2026-06-12 20:49:12 +0300

    chore: bump version

--- tests/test_miniwerk.py
+++ tests/test_miniwerk.py
@@ -5,4 +5,4 @@ from miniwerk import __version__
 
 def test_version() -> None:
     """Make sure version matches expected"""
-    assert __version__ == "1.8.2+260524"
+    assert __version__ == "1.8.2+260612"
